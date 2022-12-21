6a249e1c1bf0a17c81fed8c94700204da1eef28e
Annisagit <annisamarfadilla@gmail.com>
2022-12-21 08:50:55 +0700

    Form Studio edit datagrid

--- SistemPenjualanTiketBioskop/Studio.Designer.vb
+++ SistemPenjualanTiketBioskop/Studio.Designer.vb
@@ -22,9 +22,70 @@ Partial Class Studio
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.DataGridStudio = New System.Windows.Forms.DataGridView()
+        Me.BtnUpdate = New System.Windows.Forms.Button()
+        Me.BtnRemove = New System.Windows.Forms.Button()
+        Me.LblDataStudio = New System.Windows.Forms.Label()
+        CType(Me.DataGridStudio, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridStudio
+        '
+        Me.DataGridStudio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridStudio.Location = New System.Drawing.Point(12, 80)
+        Me.DataGridStudio.Name = "DataGridStudio"
+        Me.DataGridStudio.RowHeadersWidth = 62
+        Me.DataGridStudio.RowTemplate.Height = 33
+        Me.DataGridStudio.Size = New System.Drawing.Size(1393, 348)
+        Me.DataGridStudio.TabIndex = 0
+        '
+        'BtnUpdate
+        '
+        Me.BtnUpdate.Location = New System.Drawing.Point(1140, 458)
+        Me.BtnUpdate.Name = "BtnUpdate"
+        Me.BtnUpdate.Size = New System.Drawing.Size(112, 34)
+        Me.BtnUpdate.TabIndex = 2
+        Me.BtnUpdate.Text = "Update"
+        Me.BtnUpdate.UseVisualStyleBackColor = True
+        '
+        'BtnRemove
+        '
+        Me.BtnRemove.Location = New System.Drawing.Point(1265, 458)
+        Me.BtnRemove.Name = "BtnRemove"
+        Me.BtnRemove.Size = New System.Drawing.Size(112, 34)
+        Me.BtnRemove.TabIndex = 3
+        Me.BtnRemove.Text = "Remove"
+        Me.BtnRemove.UseVisualStyleBackColor = True
+        '
+        'LblDataStudio
+        '
+        Me.LblDataStudio.AutoSize = True
+        Me.LblDataStudio.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.LblDataStudio.Location = New System.Drawing.Point(615, 30)
+        Me.LblDataStudio.Name = "LblDataStudio"
+        Me.LblDataStudio.Size = New System.Drawing.Size(171, 32)
+        Me.LblDataStudio.TabIndex = 4
+        Me.LblDataStudio.Text = "DATA STUDIO"
+        '
+        'Studio
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1417, 511)
+        Me.Controls.Add(Me.LblDataStudio)
+        Me.Controls.Add(Me.BtnRemove)
+        Me.Controls.Add(Me.BtnUpdate)
+        Me.Controls.Add(Me.DataGridStudio)
+        Me.Name = "Studio"
         Me.Text = "Studio"
+        CType(Me.DataGridStudio, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents DataGridStudio As DataGridView
+    Friend WithEvents BtnUpdate As Button
+    Friend WithEvents BtnRemove As Button
+    Friend WithEvents LblDataStudio As Label
 End Class
--- SistemPenjualanTiketBioskop/Studio.vb
+++ SistemPenjualanTiketBioskop/Studio.vb
@@ -1,3 +1,61 @@
 ﻿Public Class Studio
+    Public Shared data_studio As DataStudio
+    Public Shared SelectedTableStudio As Integer
+    Public Shared SelectedTableNamaStudio As String
 
+
+    Private Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        ReloadDataStudioDatabase()
+    End Sub
+
+    Private Sub ReloadDataStudioDatabase()
+        DataGridStudio.DataSource = data_studio.GetDataStudioDatabase()
+    End Sub
+
+    Private Sub datagrid_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        ReloadDataStudioDatabase()
+    End Sub
+
+    Private Sub DataGridStudio_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridStudio.CellClick
+        Dim index As Integer = e.RowIndex
+        Dim selectedRow As DataGridViewRow
+        selectedRow = DataGridStudio.Rows(index)
+
+        SelectedTableStudio = selectedRow.Cells(0).Value
+        SelectedTableNamaStudio = selectedRow.Cells(1).Value
+    End Sub
+
+    'Private Sub BtnSelect_Click(sender As Object, e As EventArgs) Handles BtnSelect.Click
+    '    Dim SelectedStudio As List(Of String) = data_studio.GetDataStudioByIDDatabase(SelectedTableStudio)
+
+    '    data_studio.GSNama = SelectedStudio(1)
+    '    data_studio.GSJenis = SelectedStudio(2)
+    '    data_studio.GSKapasitas = SelectedStudio(3)
+    '    data_studio.GSHarga = SelectedStudio(4)
+
+    '    'Dim formInfo = New info_tambah_koleksi()
+    '    'formInfo.Show()
+    'End Sub
+
+    Private Sub BtnUpdate_Click(sender As Object, e As EventArgs) Handles BtnUpdate.Click
+        Dim SelectedStudio As List(Of String) = data_studio.GetDataStudioByIDDatabase(SelectedTableStudio)
+
+        data_studio.GSNama = SelectedStudio(1)
+        data_studio.GSJenis = SelectedStudio(2)
+        data_studio.GSKapasitas = SelectedStudio(3)
+        data_studio.GSHarga = SelectedStudio(4)
+
+        Dim formUpdate = New EditStudio()
+        formUpdate.Show()
+    End Sub
+
+    Private Sub BtnRemove_Click(sender As Object, e As EventArgs) Handles BtnRemove.Click
+        Dim formhapus = New HapusStudio()
+        formhapus.Show()
+    End Sub
 End Class
