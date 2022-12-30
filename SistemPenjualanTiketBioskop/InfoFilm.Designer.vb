a76a2261ebbf75857a339f72727c710c52903960
Marwah Nur Shafira <marwahnurshafira1705@gmail.com>
2022-12-30 18:15:09 +0700

    Menambahkan form info film

---
+++ SistemPenjualanTiketBioskop/InfoFilm.Designer.vb
@@ -0,0 +1,453 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class InfoFilm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PicFoto = New System.Windows.Forms.PictureBox()
+        Me.BtnTutup = New System.Windows.Forms.Button()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.LblFilm = New System.Windows.Forms.Label()
+        Me.LblRatingUsia = New System.Windows.Forms.Label()
+        Me.LblDirector = New System.Windows.Forms.Label()
+        Me.LblDeskripsi = New System.Windows.Forms.Label()
+        Me.LblDuration = New System.Windows.Forms.Label()
+        Me.LblDateRelease = New System.Windows.Forms.Label()
+        Me.LblHargaFilm = New System.Windows.Forms.Label()
+        Me.LblBahasa = New System.Windows.Forms.Label()
+        Me.ListBoxGenre = New System.Windows.Forms.ListBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PicFoto, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(189, Byte), Integer), CType(CType(165, Byte), Integer), CType(CType(119, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(397, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(170, 36)
+        Me.Label1.TabIndex = 120
+        Me.Label1.Text = "Detail Film"
+        '
+        'Label8
+        '
+        Me.Label8.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label8.Location = New System.Drawing.Point(287, 137)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(114, 25)
+        Me.Label8.TabIndex = 136
+        Me.Label8.Text = "Rating Usia"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(101, Byte), Integer), CType(CType(99, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(920, 59)
+        Me.Panel1.TabIndex = 133
+        '
+        'PicFoto
+        '
+        Me.PicFoto.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.PicFoto.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PicFoto.Location = New System.Drawing.Point(23, 83)
+        Me.PicFoto.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.PicFoto.Name = "PicFoto"
+        Me.PicFoto.Size = New System.Drawing.Size(231, 289)
+        Me.PicFoto.TabIndex = 132
+        Me.PicFoto.TabStop = False
+        '
+        'BtnTutup
+        '
+        Me.BtnTutup.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.BtnTutup.BackColor = System.Drawing.Color.Teal
+        Me.BtnTutup.FlatAppearance.BorderSize = 0
+        Me.BtnTutup.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnTutup.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.BtnTutup.ForeColor = System.Drawing.Color.White
+        Me.BtnTutup.Location = New System.Drawing.Point(397, 832)
+        Me.BtnTutup.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.BtnTutup.Name = "BtnTutup"
+        Me.BtnTutup.Size = New System.Drawing.Size(208, 37)
+        Me.BtnTutup.TabIndex = 131
+        Me.BtnTutup.Text = "Tutup"
+        Me.BtnTutup.UseVisualStyleBackColor = False
+        '
+        'Label11
+        '
+        Me.Label11.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label11.Location = New System.Drawing.Point(287, 648)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(65, 25)
+        Me.Label11.TabIndex = 129
+        Me.Label11.Text = "Genre"
+        '
+        'Label7
+        '
+        Me.Label7.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label7.Location = New System.Drawing.Point(287, 539)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(110, 25)
+        Me.Label7.TabIndex = 127
+        Me.Label7.Text = "Harga Film"
+        '
+        'Label6
+        '
+        Me.Label6.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.Location = New System.Drawing.Point(287, 490)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(129, 25)
+        Me.Label6.TabIndex = 126
+        Me.Label6.Text = "Date Release"
+        '
+        'Label5
+        '
+        Me.Label5.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(287, 435)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(89, 25)
+        Me.Label5.TabIndex = 125
+        Me.Label5.Text = "Duration"
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(287, 236)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(97, 25)
+        Me.Label4.TabIndex = 123
+        Me.Label4.Text = "Deskripsi"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(287, 187)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(85, 25)
+        Me.Label3.TabIndex = 122
+        Me.Label3.Text = "Director"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(287, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 25)
+        Me.Label2.TabIndex = 121
+        Me.Label2.Text = "Film"
+        '
+        'Label10
+        '
+        Me.Label10.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label10.Location = New System.Drawing.Point(287, 591)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(75, 25)
+        Me.Label10.TabIndex = 128
+        Me.Label10.Text = "Bahasa"
+        '
+        'LblFilm
+        '
+        Me.LblFilm.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.LblFilm.AutoSize = True
+        Me.LblFilm.Location = New System.Drawing.Point(487, 87)
+        Me.LblFilm.Name = "LblFilm"
+        Me.LblFilm.Size = New System.Drawing.Size(19, 25)
+        Me.LblFilm.TabIndex = 137
+        Me.LblFilm.Text = "-"
+        '
+        'LblRatingUsia
+        '
+        Me.LblRatingUsia.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.LblRatingUsia.AutoSize = True
+        Me.LblRatingUsia.Location = New System.Drawing.Point(487, 137)
+        Me.LblRatingUsia.Name = "LblRatingUsia"
+        Me.LblRatingUsia.Size = New System.Drawing.Size(19, 25)
+        Me.LblRatingUsia.TabIndex = 138
+        Me.LblRatingUsia.Text = "-"
+        '
+        'LblDirector
+        '
+        Me.LblDirector.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.LblDirector.AutoSize = True
+        Me.LblDirector.Location = New System.Drawing.Point(487, 187)
+        Me.LblDirector.Name = "LblDirector"
+        Me.LblDirector.Size = New System.Drawing.Size(19, 25)
+        Me.LblDirector.TabIndex = 139
+        Me.LblDirector.Text = "-"
+        '
+        'LblDeskripsi
+        '
+        Me.LblDeskripsi.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.LblDeskripsi.AutoSize = True
+        Me.LblDeskripsi.Location = New System.Drawing.Point(487, 236)
+        Me.LblDeskripsi.MaximumSize = New System.Drawing.Size(370, 370)
+        Me.LblDeskripsi.Name = "LblDeskripsi"
+        Me.LblDeskripsi.Size = New System.Drawing.Size(19, 25)
+        Me.LblDeskripsi.TabIndex = 140
+        Me.LblDeskripsi.Text = "-"
+        '
+        'LblDuration
+        '
+        Me.LblDuration.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.LblDuration.AutoSize = True
+        Me.LblDuration.Location = New System.Drawing.Point(487, 435)
+        Me.LblDuration.Name = "LblDuration"
+        Me.LblDuration.Size = New System.Drawing.Size(19, 25)
+        Me.LblDuration.TabIndex = 141
+        Me.LblDuration.Text = "-"
+        '
+        'LblDateRelease
+        '
+        Me.LblDateRelease.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.LblDateRelease.AutoSize = True
+        Me.LblDateRelease.Location = New System.Drawing.Point(487, 490)
+        Me.LblDateRelease.Name = "LblDateRelease"
+        Me.LblDateRelease.Size = New System.Drawing.Size(19, 25)
+        Me.LblDateRelease.TabIndex = 142
+        Me.LblDateRelease.Text = "-"
+        '
+        'LblHargaFilm
+        '
+        Me.LblHargaFilm.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.LblHargaFilm.AutoSize = True
+        Me.LblHargaFilm.Location = New System.Drawing.Point(487, 539)
+        Me.LblHargaFilm.Name = "LblHargaFilm"
+        Me.LblHargaFilm.Size = New System.Drawing.Size(19, 25)
+        Me.LblHargaFilm.TabIndex = 143
+        Me.LblHargaFilm.Text = "-"
+        '
+        'LblBahasa
+        '
+        Me.LblBahasa.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.LblBahasa.AutoSize = True
+        Me.LblBahasa.Location = New System.Drawing.Point(487, 591)
+        Me.LblBahasa.Name = "LblBahasa"
+        Me.LblBahasa.Size = New System.Drawing.Size(19, 25)
+        Me.LblBahasa.TabIndex = 144
+        Me.LblBahasa.Text = "-"
+        '
+        'ListBoxGenre
+        '
+        Me.ListBoxGenre.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.ListBoxGenre.FormattingEnabled = True
+        Me.ListBoxGenre.ItemHeight = 25
+        Me.ListBoxGenre.Location = New System.Drawing.Point(487, 648)
+        Me.ListBoxGenre.Name = "ListBoxGenre"
+        Me.ListBoxGenre.Size = New System.Drawing.Size(180, 154)
+        Me.ListBoxGenre.TabIndex = 145
+        '
+        'Label9
+        '
+        Me.Label9.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(443, 490)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(16, 25)
+        Me.Label9.TabIndex = 146
+        Me.Label9.Text = ":"
+        '
+        'Label12
+        '
+        Me.Label12.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(443, 435)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(16, 25)
+        Me.Label12.TabIndex = 147
+        Me.Label12.Text = ":"
+        '
+        'Label13
+        '
+        Me.Label13.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(443, 236)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(16, 25)
+        Me.Label13.TabIndex = 148
+        Me.Label13.Text = ":"
+        '
+        'Label14
+        '
+        Me.Label14.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(443, 187)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(16, 25)
+        Me.Label14.TabIndex = 149
+        Me.Label14.Text = ":"
+        '
+        'Label15
+        '
+        Me.Label15.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(443, 137)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(16, 25)
+        Me.Label15.TabIndex = 150
+        Me.Label15.Text = ":"
+        '
+        'Label16
+        '
+        Me.Label16.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(443, 87)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(16, 25)
+        Me.Label16.TabIndex = 151
+        Me.Label16.Text = ":"
+        '
+        'Label17
+        '
+        Me.Label17.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(443, 539)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(16, 25)
+        Me.Label17.TabIndex = 152
+        Me.Label17.Text = ":"
+        '
+        'Label18
+        '
+        Me.Label18.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label18.AutoSize = True
+        Me.Label18.Location = New System.Drawing.Point(443, 591)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(16, 25)
+        Me.Label18.TabIndex = 153
+        Me.Label18.Text = ":"
+        '
+        'InfoFilm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(920, 889)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.ListBoxGenre)
+        Me.Controls.Add(Me.LblBahasa)
+        Me.Controls.Add(Me.LblHargaFilm)
+        Me.Controls.Add(Me.LblDateRelease)
+        Me.Controls.Add(Me.LblDuration)
+        Me.Controls.Add(Me.LblDeskripsi)
+        Me.Controls.Add(Me.LblDirector)
+        Me.Controls.Add(Me.LblRatingUsia)
+        Me.Controls.Add(Me.LblFilm)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.PicFoto)
+        Me.Controls.Add(Me.BtnTutup)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label10)
+        Me.Name = "InfoFilm"
+        Me.Text = "Info Film"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PicFoto, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PicFoto As PictureBox
+    Friend WithEvents BtnTutup As Button
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents LblFilm As Label
+    Friend WithEvents LblRatingUsia As Label
+    Friend WithEvents LblDirector As Label
+    Friend WithEvents LblDeskripsi As Label
+    Friend WithEvents LblDuration As Label
+    Friend WithEvents LblDateRelease As Label
+    Friend WithEvents LblHargaFilm As Label
+    Friend WithEvents LblBahasa As Label
+    Friend WithEvents ListBoxGenre As ListBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label18 As Label
+End Class
