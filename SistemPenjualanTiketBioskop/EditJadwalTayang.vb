7a500954037b44c3cafcbba089930ec64f996658
Michael Natanael <mnatanael87@gmail.com>
2023-01-03 13:28:34 +0700

    Edit jadwal tayang

--- SistemPenjualanTiketBioskop/EditJadwalTayang.vb
+++ SistemPenjualanTiketBioskop/EditJadwalTayang.vb
@@ -1,3 +1,19 @@
 ﻿Public Class EditJadwalTayang
+    Public Shared data_jadwal_tayang As DataJadwalTayang
+    Public Shared data_film As DataFilm
+    Public Shared data_studio As DataStudio
 
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        data_film = New DataFilm()
+        data_studio = New DataStudio()
+        ComboBoxFilm.DataSource = data_film.GetDataFilmDatabaseList()
+        ComboBoxStudio.DataSource = data_studio.GetDataStudioDatabaseList()
+
+
+    End Sub
 End Class
