4e8e0aec1fa6515adbf10b6462963a84ce74c6e4
Marwah Nur Shafira <marwahnurshafira1705@gmail.com>
2023-01-18 09:21:11 +0700

    Add latest bioskop.sql

--- bioskop.sql
+++ bioskop.sql
@@ -1,34 +1,27 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Dec 20, 2022 at 01:12 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `bioskop`
---
-
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server version:               10.4.24-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.0.0.6468
 -- --------------------------------------------------------
 
---
--- Table structure for table `film`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE `film` (
-  `id` int(11) NOT NULL,
+
+-- Dumping database structure for bioskop
+CREATE DATABASE IF NOT EXISTS `bioskop` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `bioskop`;
+
+-- Dumping structure for table bioskop.film
+CREATE TABLE IF NOT EXISTS `film` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama_film` varchar(255) NOT NULL,
   `genre` varchar(255) NOT NULL,
   `deskripsi` longtext NOT NULL,
@@ -37,150 +30,68 @@ CREATE TABLE `film` (
   `date_release` date NOT NULL,
   `bahasa` varchar(255) NOT NULL,
   `harga_film` int(11) NOT NULL,
-  `foto` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `foto` text NOT NULL,
+  `rating_usia` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `jadwal_tayang`
---
-
-CREATE TABLE `jadwal_tayang` (
-  `id_jadwal_tayang` int(11) NOT NULL,
+-- Dumping structure for table bioskop.jadwal_tayang
+CREATE TABLE IF NOT EXISTS `jadwal_tayang` (
+  `id_jadwal_tayang` int(11) NOT NULL AUTO_INCREMENT,
   `id_film` int(11) NOT NULL,
   `id_studio` int(11) NOT NULL,
   `tanggal` date NOT NULL,
   `waktu_mulai` time NOT NULL,
-  `waktu_selesai` time NOT NULL
+  `waktu_selesai` time NOT NULL,
+  PRIMARY KEY (`id_jadwal_tayang`),
+  KEY `fk_film` (`id_film`),
+  KEY `fk_studio` (`id_studio`),
+  CONSTRAINT `fk_film` FOREIGN KEY (`id_film`) REFERENCES `film` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_studio` FOREIGN KEY (`id_studio`) REFERENCES `studio` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `studio`
---
-
-CREATE TABLE `studio` (
-  `id` int(11) NOT NULL,
+-- Dumping structure for table bioskop.studio
+CREATE TABLE IF NOT EXISTS `studio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_studio` varchar(255) NOT NULL DEFAULT '0',
+  `jenis_studio` varchar(255) NOT NULL DEFAULT '0',
   `kapasitas` int(11) NOT NULL,
-  `harga_kursi` int(11) NOT NULL
+  `harga_kursi` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `tiket`
---
-
-CREATE TABLE `tiket` (
-  `id` int(11) NOT NULL,
+-- Dumping structure for table bioskop.tiket
+CREATE TABLE IF NOT EXISTS `tiket` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_jadwal_tayang` int(11) NOT NULL,
-  `total_harga` int(20) NOT NULL
+  `total_harga` int(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_jadwal_tayang` (`id_jadwal_tayang`),
+  CONSTRAINT `fk_jadwal_tayang` FOREIGN KEY (`id_jadwal_tayang`) REFERENCES `jadwal_tayang` (`id_jadwal_tayang`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(255) NOT NULL,
+-- Dumping structure for table bioskop.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `password` varchar(255) NOT NULL,
+  `registered_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
+-- Data exporting was unselected.
 
---
--- Indexes for table `film`
---
-ALTER TABLE `film`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `jadwal_tayang`
---
-ALTER TABLE `jadwal_tayang`
-  ADD PRIMARY KEY (`id_jadwal_tayang`),
-  ADD KEY `fk_film` (`id_film`),
-  ADD KEY `fk_studio` (`id_studio`);
-
---
--- Indexes for table `studio`
---
-ALTER TABLE `studio`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tiket`
---
-ALTER TABLE `tiket`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_jadwal_tayang` (`id_jadwal_tayang`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `film`
---
-ALTER TABLE `film`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `jadwal_tayang`
---
-ALTER TABLE `jadwal_tayang`
-  MODIFY `id_jadwal_tayang` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `studio`
---
-ALTER TABLE `studio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tiket`
---
-ALTER TABLE `tiket`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `jadwal_tayang`
---
-ALTER TABLE `jadwal_tayang`
-  ADD CONSTRAINT `fk_film` FOREIGN KEY (`id_film`) REFERENCES `film` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_studio` FOREIGN KEY (`id_studio`) REFERENCES `studio` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `tiket`
---
-ALTER TABLE `tiket`
-  ADD CONSTRAINT `fk_jadwal_tayang` FOREIGN KEY (`id_jadwal_tayang`) REFERENCES `jadwal_tayang` (`id_jadwal_tayang`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
